9334b9e0bde514499c7dea4f2d92687262ade4a4
zslazar <zs.lazar@gmail.com>
2017-12-14 17:42:51 +0100

    new task error handling

---
+++ ToDoApp/ToDoApp/Printer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoApp
+{
+    class Printer
+    {
+        public void PrintInfo()
+        {
+            string path = @"c:\greenfox\zslazar-todoApp\info.txt";
+            {
+                string[] content = File.ReadAllLines(path);
+                foreach (string line in content)
+                Console.WriteLine(line);
+            }
+        }
+
+        public void PrintTask()
+        {
+            string path = @"c:\greenfox\zslazar-todoApp\data.txt";
+            {
+                string[] content = File.ReadAllLines(path);
+                if (content.Length == 0)
+                {
+                    Console.WriteLine("No todos for today! :)");
+                }
+                foreach (string line in content)
+                Console.WriteLine(line);
+            }
+        }
+
+        public void AddLine(string line)
+        {
+            string path = @"c:\greenfox\zslazar-todoApp\data.txt";
+            StreamWriter input = new StreamWriter(path, true);
+                      
+            input.WriteLine(line);
+            input.Close();
+        }
+
+    }
+}
---
+++ ToDoApp/ToDoApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace ToDoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var printer = new Printer();
+
+            if (args.Length == 0)
+            {
+                printer.PrintInfo();
+            }
+
+            if (args[0] == "-l")
+            {
+                printer.PrintTask();
+            }
+
+            if (args[0] == "-a")
+            {
+                try
+                {
+
+                printer.AddLine(args[1]);
+                }
+                catch (System.IndexOutOfRangeException)
+                {
+
+                    Console.WriteLine("Unable to add: no task provided");
+                }
+                
+            }
+
+            //foreach (var text in args)
+            //{
+            //Console.WriteLine(text);
+            //}
+        }
+    }
+}
